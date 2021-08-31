f9244df7837e4525de01aee0a8d952bbd2c9e74e
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2021-08-31 12:11:45 +0530

    Update ec2_launch_template.tf

--- ec2_launch_template.tf
+++ ec2_launch_template.tf
@@ -19,3 +19,62 @@ resource "aws_instance" "Mynode1" {
       instance_initiated_shutdown_behavior = 
       instance_state                       = 
      }
+
+capacity_reservation_specification {
+           capacity_reservation_preference = 
+
+           capacity_reservation_target {
+           capacity_reservation_id = 
+            }
+        }
+
+ebs_block_device {
+         delete_on_termination = (known after apply)
+         device_name           = (known after apply)
+         encrypted             = (known after apply)
+         iops                  = (known after apply)
+         kms_key_id            = (known after apply)
+         snapshot_id           = (known after apply)
+         tags                  = (known after apply)
+         throughput            = (known after apply)
+         volume_id             = (known after apply)
+         volume_size           = (known after apply)
+         volume_type           = (known after apply)
+        }
+
+enclave_options {
+         enabled = 
+        }
+
+ephemeral_block_device {
+          device_name  = 
+          no_device    = 
+          virtual_name = 
+        }
+
+metadata_options {
+           http_endpoint               = 
+           http_put_response_hop_limit = 
+           http_tokens                 = 
+        }
+
+network_interface {
+           delete_on_termination = 
+           device_index          = 
+           network_interface_id  = 
+        }
+
+root_block_device {
+        delete_on_termination = 
+        device_name           = 
+        encrypted             = 
+        iops                  = 
+        kms_key_id            = 
+        tags                  = 
+        throughput            = 
+        volume_id             = 
+        volume_size           = 
+        volume_type           = 
+        }
+    }
+
