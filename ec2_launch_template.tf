54ada8a6749fafa4ef8e0490ee09495332e6b993
Sameer Deshmukh <89735172+SS0x44@users.noreply.github.com>
2021-08-31 12:06:16 +0530

    Update ec2_launch_template.tf

--- ec2_launch_template.tf
+++ ec2_launch_template.tf
@@ -1,1 +1,21 @@
-
+resource "aws_instance" "Mynode1" {
+      ami                                  = "ami-06a0b4e3b7eb7a300"
+      instance_type                        = "t2.micro"
+      key_name                             = 
+      monitoring                           = 
+      availability_zone                    = 
+	    private_ip                           =
+      secondary_private_ips                = 
+      public_ip                            =
+      associate_public_ip_address          = 
+      security_groups                      = 
+      source_dest_check                    = true
+      subnet_id                            = 
+      tags_all                             = 
+      tenancy                              = 
+      vpc_security_group_ids               = 
+      disable_api_termination              = 
+      ebs_optimized                        = 
+      instance_initiated_shutdown_behavior = 
+      instance_state                       = 
+     }
